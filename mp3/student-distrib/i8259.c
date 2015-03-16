70c41787f39fed165398d8943118b95bb283b038
Charles Anderson <charlieanderson13@gmail.com>
2015-03-16 01:44:30 -0500

    some pic/rtc stuff

--- mp3/student-distrib/i8259.c
+++ mp3/student-distrib/i8259.c
@@ -10,27 +10,88 @@
 uint8_t master_mask; /* IRQs 0-7 */
 uint8_t slave_mask; /* IRQs 8-15 */
 
+uint8_t master_data = (MASTER_8259_PORT+1);
+uint8_t slave_data = (SLAVE_8259_PORT+1);
+
 /* Initialize the 8259 PIC */
 void
 i8259_init(void)
-{
+{												//based off of wiki.osdev.org ===============================================================================================================================>
+
+	uint16_t mask1 = inb(master_data);
+	uint16_t mask2 = inb(slave_data);
+
+														//THESE MIGHT ALL BE REVERESED, like ICW then port... due to different syntax GAS
+	outb( ICW1, MASTER_8259_PORT);  
+	outb( ICW1, SLAVE_8259_PORT);
+
+
+
+	outb( ICW2_MASTER, master_data);                 
+	outb( ICW2_SLAVE, slave_data);                 
+
+
+	outb( ICW3_MASTER, master_data);                       
+	outb( ICW3_SLAVE, slave_data);                      
+ 
+
+	outb( ICW4, master_data);
+	outb( ICW4, slave_data);
+ 
+
+	outb( mask1, master_data);  
+	outb( mask2, slave_data);
+
+
+
 }
 
 /* Enable (unmask) the specified IRQ */
 void
 enable_irq(uint32_t irq_num)
 {
+	uint16_t port;
+    uint8_t value;
+ 
+    if(irq_num < 8) {
+        port = master_data;
+    } else {
+        port = slave_data;
+        irq_num -= 8;
+    }
+    value = inb(port) & ~(1 << irq_num);
+    outb( value, port);
 }
 
 /* Disable (mask) the specified IRQ */
 void
 disable_irq(uint32_t irq_num)
 {
+	uint16_t port;
+    uint8_t value;
+ 
+    if(irq_num < 8) {
+        port = master_data;
+    } else {
+        port = slave_data;
+        irq_num -= 8;
+    }
+    value = inb(port) | (1 << irq_num);
+    outb(value, port );
 }
 
 /* Send end-of-interrupt signal for the specified IRQ */
 void
 send_eoi(uint32_t irq_num)
 {
+
+//OR EOI with irq num and send it out to PIC
+uint8_t irq_cmd = irq_num | EOI;
+
+	if(irq_num >= 8)
+		outb( irq_cmd, SLAVE_8259_PORT);
+ 
+	outb(irq_cmd, MASTER_8259_PORT); //might change second param for both, it should be the command to send
+
 }
 
--- mp3/student-distrib/kernel.c
+++ mp3/student-distrib/kernel.c
@@ -8,6 +8,7 @@
 #include "i8259.h"
 #include "debug.h"
 #include "keyboard.h"
+#include "idt_init.h"
 
 /* Macros. */
 /* Check if the bit BIT in FLAGS is set. */
@@ -147,12 +148,34 @@ entry (unsigned long magic, unsigned long addr)
 
 	/* Init the idt */
 	idt_init();
-	
+
 	/* Init the PIC */
 	i8259_init();
 
+
 	/* Init the keyboard */
 	enable_irq(KEYBOARD_IRQ);
+
+
+//RTC INIT
+//RTC interrupts are disabled by default. If you turn on the RTC interrupts, the RTC will periodically generate IRQ 8.
+char prev;
+//cli();			// disable interrupts
+//CMOS I/O ports are 0x70,0x71
+outb( 0x8B, 0x70);		// 0xB is the offset for register B, might have to reset A,B and C to start?
+prev = inb(0x71);	
+outb( 0x8B, 0x70);		// done again because reads will reset it
+outb( prev|0x40, 0x71);	// setting bit 6 of register B of CMOS, which is periodic interrupt
+///sti()   enable interrupts
+
+
+//filename: interrupt_handler.c 
+/*
+void interrupt_handler()
+{
+  
+}
+*/
 
 	/* Initialize devices, memory, filesystem, enable device interrupts on the
 	 * PIC, any other initialization stuff... */
